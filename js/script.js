99b73d97fcae4f1ac80dd2f00dad9ce1ba272f53
Arif <iariful950@gmail.com>
2022-03-01 03:20:01 +0600

    search results section done!

---
+++ js/script.js
@@ -0,0 +1,62 @@
+// get search input
+const searchResults = () => {
+    document.getElementById('spinner').style.display = 'block';
+    // document.getElementById('details-div').style.display = 'none';
+    const searchText = document.getElementById('search-input').value;
+    // checking some cases
+    if (!isNaN(searchText) || searchText == '') {
+        document.getElementById('warning').style.display = 'block';
+        document.getElementById('spinner').style.display = 'none';
+        document.getElementById('phones').style.display = 'none';
+    }
+    else {
+        loadPhones(searchText);
+    }
+    document.getElementById('search-input').value = '';
+}
+
+// load phones using api
+const loadPhones = searchText => {
+    let url = 'https://openapi.programming-hero.com/api/phones?search=';
+    url += searchText;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayPhones(data.data));
+}
+
+// display phones 
+const displayPhones = phones => {
+    // console.log(phones);
+    // checking some cases
+    if (phones.length == 0) {
+        document.getElementById('no-match').style.display = 'block';
+        document.getElementById('spinner').style.display = 'none';
+        document.getElementById('warning').style.display = 'none';
+        document.getElementById('phones').style.display = 'none';
+        // document.getElementById('details-div').style.display = 'none';
+    }
+    else {
+        document.getElementById('warning').style.display = 'none';
+        document.getElementById('no-match').style.display = 'none';
+        document.getElementById('spinner').style.display = 'none';
+        // document.getElementById('details-div').style.display = 'none';
+        // console.log(phones);
+        const allPhonesDiv = document.getElementById('phones');
+        allPhonesDiv.textContent = '';
+        phones.slice(0, 20).forEach(phone => {
+            // console.log(phone);
+            const phoneDiv = document.createElement('div');
+            phoneDiv.classList.add('phone-div-style');
+            phoneDiv.innerHTML = `
+                <img class="rounded-md pt-4" src="${phone.image}">
+                <h2 class="text-xl lg:text-2xl font-medium text-zinc-700 pt-4">Brand: ${phone.brand}</h2>
+                <h2 class="text-xl lg:text-2xl font-medium text-zinc-700 pt-4">${phone.phone_name}</h2>
+                <button class="text-lg lg:text-xl font-medium text-gray-800 rounded-md my-4 px-5 py-2 bg-gray-300  hover:bg-gray-400">View Details</button>
+            `;
+            allPhonesDiv.appendChild(phoneDiv);
+        })
+    }
+}
+
+
+
